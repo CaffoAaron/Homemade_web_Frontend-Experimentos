2e0a03933832c222cf06b5f3c071987d618f3bcd
Pacoros98 <65671832+Pacoros98@users.noreply.github.com>
2020-11-09 15:36:12 -0600

    modificaciones esteticas de la receta
    La clase receta 2 es algo con lo que estoy experimentando para mejorar la vista de la receta

--- src/components/receta.vue
+++ src/components/receta.vue
@@ -1,14 +1,15 @@
 <template>
   <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+    <br><br>
     <v-timeline-item color="purple lighten-2" fill-dot right>
       <v-card>
         <v-card-title class="purple lighten-2">
           <!--v-icon dark size="42" class="mr-4">
             mdi-magnify
           </v-icon-->
-          <h2 class="display-1 white--text font-weight-light">
+          <h1 class="display-1 white--text font-weight-light">
             Ceviche con pollo:
-          </h2>
+          </h1>
         </v-card-title>
         <v-container>
           <v-row>
@@ -16,21 +17,23 @@
               El Ceviche de pollo es un plato originario de la región costera del norte de Perú, donde se macera esta carne con jugo de limón, ajíes y otras especias. Sin embargo, también se hace variando la receta en países como Ecuador o Chile o en diferentes zonas del perú.
             </v-col>
             <v-col class="hidden-sm-and-down text-right" md="2">
-              <v-icon size="64">
+              <!--v-icon size="64">
                 mdi-calendar-text
-              </v-icon>
+              </v-icon-->
             </v-col>
           </v-row>
         </v-container>
       </v-card>
     </v-timeline-item>
 
+    <br><br>
+
     <v-timeline-item color="amber lighten-1" fill-dot left small>
       <v-card>
         <v-card-title class="amber lighten-1 justify-end">
-          <h2 class="display-1 mr-4 white--text font-weight-light">
+          <h1 class="display-1 mr-4 white--text font-weight-light">
             Ingredientes:
-          </h2>
+          </h1>
           <!--v-icon dark size="42">
             mdi-home-outline
           </v-icon-->
@@ -78,15 +81,17 @@
       </v-card>
     </v-timeline-item>
 
+    <br><br>
+
     <v-timeline-item color="cyan lighten-1" fill-dot right>
       <v-card>
         <v-card-title class="cyan lighten-1">
           <!--v-icon class="mr-4" dark size="42">
             mdi-email-outline
           </v-icon-->
-          <h2 class="display-1 white--text font-weight-light">
+          <h1 class="display-1 white--text font-weight-light">
             Procedimiento
-          </h2>
+          </h1>
         </v-card-title>
         <v-container>
           <ol>
@@ -113,13 +118,14 @@
       </v-card>
     </v-timeline-item>
 
+    <br><br>
 
     <v-timeline-item color="red lighten-1" fill-dot left small>
       <v-card>
         <v-card-title class="red lighten-1 justify-end">
-          <h2 class="display-1 mr-4 white--text font-weight-light">
+          <h1 class="display-1 mr-4 white--text font-weight-light">
             Consejos del chef:
-          </h2>
+          </h1>
           <!--v-icon dark size="42">
             mdi-home-outline
           </v-icon-->
@@ -130,59 +136,19 @@
               <span style="font-weight: 400;">Las cantidades mostradas dan para unas cuatro raciones y entre la preparación y el marinaje esta receta apenas nos tomará media hora de nuestro tiempo. El resultado final superará tus expectativas, ya que su sabor y textura es un auténtico placer para el paladar. ¡Y sin apenas calorías! Por lo que se trata de un alimento muy adecuado para todo tipo de dietas balanceadas y de adelgazamiento. </span>
             </li>
             <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Dos cebollas.</span>
+              <span style="font-weight: 400;">Además, la carne de pollo posee grandes ventajas nutricionales, con un 20% más de proteínas que la carne de vaca y menos grasas. Apenas tiene hidratos de carbonos, es muy suave y se digiere de maravilla. Es una fuente natural de minerales como el calcio, fósforo, hierro, zinc, magnesio, sodio y potasio, vitaminas A, B y C, ácido fólico y otras sustancias muy beneficiosas para el organismo. </span>
             </li>
             <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Cinco o seis limones.</span>
+              <span style="font-weight: 400;">Como todos los ceviches, puedes variar los ingredientes dependiendo de tus gustos y la disponibilidad que tengas en la cocina. Por ejemplo, prueba a añadir un poco de vinagre, chicha de jora o jugo de naranja para variar el nivel de acidez, usa perejil en lugar de cilantro o acompaña el plato con papas, yucas o rodajas de camote. De hecho, hay quien</span>
             </li>
             <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Dos ajos.</span>
-            </li>
-            <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Cinco cucharadas de ají amarillo molido, también llamado ají escabeche.</span>
-            </li>
-            <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Una cucharada de ají panca molido, aunque en Perú es habitual encontrarlo como ají colorado, especial o seco.</span>
-            </li>
-            <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Medio ají amarillo.</span>
-            </li>
-            <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Media taza de caldo de pollo, aunque si no tienes puedes usar agua en su lugar.</span>
-            </li>
-            <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Una rama de cilantro o culantro.</span>
-            </li>
-            <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Una cucharada de aceite vegetal.</span>
-            </li>
-            <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Sal y pimienta.</span>
-            </li>
-            <li style="font-weight: 400;">
-              <span style="font-weight: 400;">Opcional: una hoja de lechuga y arroz blanco.</span>
+              <span style="font-weight: 400;">Si no puedes encontrar los ajíes molidos que hemos mencionado en la receta, puedes preparar una pasta casera siguiendo estos pasos: Corta dos ajíes amarillos por la mitad y quítales los septos, las semillas y los corazones. Cocínalos en agua caliente cambiando de líquido tres veces, de esta forma reduciremos considerablemente el picor y acidez de los ajíes y el sabor será mucho más delicado.</span>
             </li>
           </ul>
         </v-container>
       </v-card>
     </v-timeline-item>
-
-    <H1>Consejos del chef: </H1>
-
-    <ul>
-      <li style="font-weight: 400;">
-      </li>
-      <li style="font-weight: 400;">
-      </li>
-      <li style="font-weight: 400;">
-        <span style="font-weight: 400;">Como todos los ceviches, puedes variar los ingredientes dependiendo de tus gustos y la disponibilidad que tengas en la cocina. Por ejemplo, prueba a añadir un poco de vinagre, chicha de jora o jugo de naranja para variar el nivel de acidez, usa perejil en lugar de cilantro o acompaña el plato con papas, yucas o rodajas de camote. De hecho, hay quien</span>
-      </li>
-      <li style="font-weight: 400;">
-        <span style="font-weight: 400;">Si no puedes encontrar los ajíes molidos que hemos mencionado en la receta, puedes preparar una pasta casera siguiendo estos pasos: Corta dos ajíes amarillos por la mitad y quítales los septos, las semillas y los corazones. Cocínalos en agua caliente cambiando de líquido tres veces, de esta forma reduciremos considerablemente el picor y acidez de los ajíes y el sabor será mucho más delicado.</span>
-      </li>
-    </ul>
     <br><br>
-    <hr>
     <br><br>
   </v-timeline>
 </template>
