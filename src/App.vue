57526276f5550d7bc7cd2c928be5e5d5a438ec70
Victor Fajardo <victor.fajardo.rojas@gmail.com>
2020-11-09 22:57:31 -0500

    profile picture now redirect to home-chef

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,14 @@
   <v-app>
     <v-app-bar app color="orange" flat>
       <v-container class="py-0 fill-height">
-        <v-avatar
-            class="mr-10"
-            color="grey darken-1"
-            size="32"
-        >
-          <v-img
-              height="65"
-              src="https://scontent.flim18-1.fna.fbcdn.net/v/t1.0-9/107697646_1063570910706020_6049356131090790239_o.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeElQJo0XMR59x1fWdDj4MHVhBRN2sNJSP2EFE3aw0lI_QgZw6_XxVcq_ynfVGuQcPxodxUpVNBNu-4VqBBAXEEW&_nc_ohc=i-oqCeMNN1oAX--4BkU&_nc_ht=scontent.flim18-1.fna&oh=d57aed443b5f97a858f228b8fbf3de64&oe=5FCEF49A"
-          ></v-img>
-        </v-avatar>
+          <router-link to="/">
+             <v-img
+                     src="https://scontent.flim18-1.fna.fbcdn.net/v/t1.0-9/107697646_1063570910706020_6049356131090790239_o.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeElQJo0XMR59x1fWdDj4MHVhBRN2sNJSP2EFE3aw0lI_QgZw6_XxVcq_ynfVGuQcPxodxUpVNBNu-4VqBBAXEEW&_nc_ohc=i-oqCeMNN1oAX--4BkU&_nc_ht=scontent.flim18-1.fna&oh=d57aed443b5f97a858f228b8fbf3de64&oe=5FCEF49A"
+                     class="rounded-circle mr-4"
+                     max-height="45"
+                     max-width="45"
+             ></v-img>
+           </router-link>
         <v-toolbar-title class="text-h4">Homemade</v-toolbar-title>
 
         <v-divider
